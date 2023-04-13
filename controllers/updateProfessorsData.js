d5a7884fd4572fb80f516ab006aee6f8e33dfe61
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-04-14 00:37:25 +0800

    FINALLY FIXED LEGIT NO TAKIP 100%

--- controllers/updateProfessorsData.js
+++ controllers/updateProfessorsData.js
@@ -1,28 +1,18 @@
 const updateProfessorsData = async (req, res, db) => {
   console.log(req.body);
+  const professors = req.body;
+  const professorObjects = professors.map((professor) => ({
+    last_name: professor.lastname,
+    first_name: professor.firstname,
+    middle_name: professor.middlename,
+    employment: professor.employment,
+    max_units: professor.maxUnits,
+  }));
 
-  try {
-    const updatedProfessors = req.body;
-
-    // Loop through the updatedProfessors array and update each professor in the database
-    for (const professor of updatedProfessors) {
-      await db('professors')
-        .where({
-          first_name: professor.firstname,
-          last_name: professor.lastname,
-          middle_name: professor.middlename,
-        })
-        .update({
-          employment: professor.employment,
-          max_units: professor.maxUnits,
-        });
-    }
-
-    res.json({ success: true });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ success: false, message: 'Failed to update professors' });
-  }
+  console.log(professorObjects);
+  await db('professors').truncate(); // Clear the professors table before inserting new data
+  await db('professors').insert(professorObjects);
+  res.status(200).json({ message: 'Professor data updated successfully.' });
 };
 
 export default updateProfessorsData;
