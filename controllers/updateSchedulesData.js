6c788e52256145b7dfbf2ddf10ebb186280688df
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-04-15 06:02:47 +0800

    FIXED LEGIT I CLAIM IT THANKS LORD!

--- controllers/updateSchedulesData.js
+++ controllers/updateSchedulesData.js
@@ -2,7 +2,15 @@ const updateSchedulesData = async (req, res, db) => {
   console.log(req.body, 'hahah');
   const schedules = req.body;
 
+  
+
+for (const schedule of schedules) {
+  console.log(typeof(schedule.id), typeof(parseInt(schedule.actualUnits, 10)), typeof(schedule.year), typeof(schedule.block))
+}
+
   const schedulesObjects = schedules.map((schedule) => ({
+
+    
     id: schedule.id,
     color: schedule.color,
     start_date: schedule.startDate,
@@ -13,15 +21,25 @@ const updateSchedulesData = async (req, res, db) => {
     course_name: schedule.courseName,
     course_code: schedule.courseCode,
     units: schedule.units,
-    actual_units: schedule.actualUnits,
+    actual_units: parseInt(schedule.actualUnits, 10),
     class_type: schedule.classType,
     room: schedule.room,
     day: schedule.day
   }));
 
-  console.log(schedulesObjects, 'hahha');
+  try {
     await db('schedules').truncate();
-    await db('schedules').insert(schedulesObjects);
+
+    if (schedulesObjects.length > 0) {
+      await db('schedules').insert(schedulesObjects);
+    }
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: 'Failed to update schedules' });
+  }
+
 
 };
 
