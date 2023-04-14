f63c0b2882a7a54069fca337ba1372d9838e975d
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-04-15 03:34:30 +0800

    FIXED MANDELA MAYBE

--- controllers/updateSchedulesData.js
+++ controllers/updateSchedulesData.js
@@ -2,11 +2,6 @@ const updateSchedulesData = async (req, res, db) => {
   console.log(req.body, 'hahah');
   const schedules = req.body;
 
-  if (!schedules || schedules.length === 0) {
-    res.status(400).json({ error: 'No schedules data received' });
-    return;
-  }
-
   const schedulesObjects = schedules.map((schedule) => ({
     id: schedule.id,
     color: schedule.color,
@@ -25,14 +20,9 @@ const updateSchedulesData = async (req, res, db) => {
   }));
 
   console.log(schedulesObjects, 'hahha');
-  try {
     await db('schedules').truncate();
     await db('schedules').insert(schedulesObjects);
-    res.status(200).json({ message: 'Schedules updated successfully' });
-  } catch (error) {
-    console.error('Error updating schedules:', error);
-    res.status(400).json({ error: 'Error updating schedules' });
-  }
+
 };
 
 export default updateSchedulesData;
