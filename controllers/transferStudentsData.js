3434620c255c1ea2c3076b8a3689122f20edfef5
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-05-08 02:41:45 +0800

    convert success

--- controllers/transferStudentsData.js
+++ controllers/transferStudentsData.js
@@ -1,29 +1,26 @@
 const transferStudentsData = async (req, res, db) => {
   
-    let studentId = req.query.studentId;
-    if (studentId.length > 4 && studentId.charAt(4) !== '-') {
-      const newStudentId = studentId.slice(0, 4) + '-' + studentId.slice(4);
-      studentId = newStudentId;
-    }
+  let studentId = req.query.studentId;
+  if (studentId.length > 4 && studentId.charAt(4) !== '-') {
+    const newStudentId = studentId.slice(0, 4) + '-' + studentId.slice(4);
+    studentId = newStudentId;
+  }
 
-    console.log('Student ID:', studentId);
-    
-    try {
-      const query = db('students')
-        .select(db.raw("last_name || ', ' || first_name || ' ' || middle_name as full_name"))
-        .where('student_id', '=', studentId)
-        .orderBy('last_name', 'asc');
-      
-    
+  console.log('Student ID:', studentId);
+  
+  try {
+    const query = db('students')
+      .select('last_name', 'first_name', 'middle_name')
+      .where('student_id', '=', studentId)
+      .orderBy('last_name', 'asc');
 
-      const fullNames = await query;
-    
-      res.status(200).json(fullNames);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'An error occurred while retrieving student data.' });
-    }
-  };
+    const studentData = await query;
   
-  export default transferStudentsData;
-  
+    res.status(200).json(studentData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'An error occurred while retrieving student data.' });
+  }
+};
+
+export default transferStudentsData;
